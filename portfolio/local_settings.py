971d3ab61c06dd61fa1dc553affd16940fa06976
eljasuhonen <40121627+eljasuhonen@users.noreply.github.com>
2019-12-19 15:59:09 +0200

    Create local_settings.py

---
+++ portfolio/local_settings.py
@@ -0,0 +1,15 @@
+
+SECRET_KEY = '8re%p-kep8doesnot!ub_bpe39tf5!ubelonghere^g9rdfn(h&51^g2kkk93s-%lzb'
+
+ALLOWED_HOSTS = ['139.59.152.215']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'portfoliodb',
+        'USER':'postgres',
+        'PASSWORD':'EljalearnsDjango92',
+        'HOST':'localhost',
+        'PORT':'5432',
+    }
+}

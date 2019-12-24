5cc88ad9bb6d5571638cf65a28ec92c62fb82a8a
Elja <eljailari.suhonen@gmail.com>
2019-12-24 18:11:55 +0200

    staticfile command fix

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
-    'jxobs.apps.JobsConfig',
+    'jobs.apps.JobsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
